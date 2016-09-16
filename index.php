2e0e108715e93b4ed1edc2631f425cdbcd702413
ssbrgn <rgn@uol.com.br>
2016-09-16 12:25:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,20 @@
-<?php
-    // configuration
-    require("../includes/config.php");
-    
-    // Query current users shares
-    $rows = CS50::query("SELECT symbol, shares, avgprice FROM portfolio WHERE user_id = ?", $_SESSION["id"]);
-    
-    // Construnct the view
-    $shares = [];
-    
-    foreach($rows as $row)
-    {
-        // Query the Yahoo
-        $stock = lookup($row["symbol"]);
-        
-        if ($stock !== false)
-        {
-            $shares[] = [
-                "symbol"    => $row["symbol"]
-                ,"name"     => $stock["name"]
-                ,"shares"   => $row["shares"]
-                ,"price_bought" => $row["avgprice"]
-                ,"price_cur"    => $stock["price"]
-                ,"total_bought" => $row["shares"] * $row["avgprice"]
-                ,"total_cur"    => $row["shares"] * $stock["price"]
-                ,"profit"   => $row["shares"] * $stock["price"] - $row["shares"] * $row["avgprice"]               
-            ];
-        }
-    }
-    
-    $query = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
-    $upcash = $query[0];
-    $_SESSION["cash"] = $upcash["cash"]; 
-    // render portfolio
-    render("portfolio.php", ["title" => "Portfolio", "username" => $_SESSION["username"], "cash" => $_SESSION["cash"], "shares" => $shares]);
-?>
+<!DOCTYPE html>
+<form action="login.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="password" placeholder="Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Log In
+            </button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    or <a href="register.php">register</a> for an account
+</div>
